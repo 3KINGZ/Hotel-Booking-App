785be47e515e24142e5c6d0bf167828478856575
3kingz <victorkenneth95@gmail.com>
2021-06-15 08:10:56 +0100

    removed grey background on bottom tab navigation

--- src/navigation/AppTabNavigator.tsx
+++ src/navigation/AppTabNavigator.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/display-name */
 import React from "react";
+import { View } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import HomeStackNavigator from "./HomeStackNavigator";
@@ -12,55 +13,57 @@ const Tab = createBottomTabNavigator();
 
 const AppTabNavigator = () => {
   return (
-    <Tab.Navigator
-      tabBarOptions={{
-        style: {
-          backgroundColor: COLORS.buttonColor,
-          width: "85%",
-          alignSelf: "center",
-          borderRadius: 10,
-          marginBottom: 10,
-          marginTop: 10,
-        },
-        showLabel: false,
-      }}>
-      <Tab.Screen
-        name={routes.HOME}
-        component={HomeStackNavigator}
-        options={{
-          tabBarIcon: ({ focused }) => (
-            <TabBarIcon name="compass-outline" active={focused} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name={routes.LOCATION}
-        component={LocationScreen}
-        options={{
-          tabBarIcon: ({ focused }) => (
-            <TabBarIcon name="location-outline" active={focused} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name={routes.PICTURES}
-        component={PicturesScreen}
-        options={{
-          tabBarIcon: ({ focused }) => (
-            <TabBarIcon name="camera-outline" active={focused} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name={routes.BOOKMARK}
-        component={BookmarkScreen}
-        options={{
-          tabBarIcon: ({ focused }) => (
-            <TabBarIcon name="bookmark-outline" active={focused} />
-          ),
-        }}
-      />
-    </Tab.Navigator>
+    <View style={{ backgroundColor: "white", flex: 1 }}>
+      <Tab.Navigator
+        tabBarOptions={{
+          style: {
+            backgroundColor: COLORS.buttonColor,
+            width: "85%",
+            alignSelf: "center",
+            borderRadius: 10,
+            marginBottom: 10,
+            marginTop: 10,
+          },
+          showLabel: false,
+        }}>
+        <Tab.Screen
+          name={routes.HOME}
+          component={HomeStackNavigator}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <TabBarIcon name="compass-outline" active={focused} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name={routes.LOCATION}
+          component={LocationScreen}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <TabBarIcon name="location-outline" active={focused} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name={routes.PICTURES}
+          component={PicturesScreen}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <TabBarIcon name="camera-outline" active={focused} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name={routes.BOOKMARK}
+          component={BookmarkScreen}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <TabBarIcon name="bookmark-outline" active={focused} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </View>
   );
 };
 
